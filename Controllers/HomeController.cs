d93f3844ecd7f7ccba1f2107c6af4a100d616b45
PhongMX <xuanphongit1109@gmail.com>
2025-03-19 20:05:37 +0700

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿namespace Todo.Controllers
+{
+    // Controllers/HomeController.cs
+    using Microsoft.AspNetCore.Mvc;
+    using Todo.Model;
+
+    public class HomeController : Controller
+    {
+        private static List<TodoItem> _todos = new List<TodoItem>();
+        private static int _nextId = 1;
+
+        public IActionResult Index()
+        {
+            return View(_todos);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title)
+        {
+            if (!string.IsNullOrEmpty(title))
+            {
+                _todos.Add(new TodoItem { Id = _nextId++, Title = title, IsCompleted = false });
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var todo = _todos.FirstOrDefault(t => t.Id == id);
+            if (todo != null)
+            {
+                _todos.Remove(todo);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ToggleComplete(int id)
+        {
+            var todo = _todos.FirstOrDefault(t => t.Id == id);
+            if (todo != null)
+            {
+                todo.IsCompleted = !todo.IsCompleted;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Model/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Model
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
